c567e4b4bc278e35eea8b6dc3dd146cb6faf270e
Rodion Bolkunevych <rbolkunevych@mindk.com>
2021-04-14 10:01:04 +0300

    feat(telegram): add module for tg notify

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,16 +16,31 @@ import { ReviewService } from './review.service';
 import { ReviewModel } from './review.model';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtGuard } from '../auth/guards/jwt.guard';
-import { IdValidationPipe } from "../pipes/ad-validation.pipe";
+import { IdValidationPipe } from '../pipes/ad-validation.pipe';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return await this.reviewService.create(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('notify')
+  async notify(@Body() dto: CreateReviewDto) {
+    const message =
+      `Имя: ${dto.name}\n` +
+      `Заголовок: ${dto.title}\n` +
+      `Описание: ${dto.description}\n` +
+      `Идентификатор продукта: ${dto.productId}\n`;
+    return await this.telegramService.sendMessage(message);
   }
 
   @UseGuards(JwtGuard)
@@ -39,9 +54,7 @@ export class ReviewController {
 
   // @UseGuards(JwtGuard)
   @Get('byProduct/:productId')
-  async getByProductId(
-    @Param('productId', IdValidationPipe) productId: string
-  ) {
+  async getByProductId(@Param('productId', IdValidationPipe) productId: string) {
     return await this.reviewService.findByProductId(productId);
   }
 }
